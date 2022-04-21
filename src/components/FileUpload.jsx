2fdee71cb42ace4a85992ee7dcb097c869cb3a7c
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-04-21 17:45:06 +0500

    result observer start

--- src/components/FileUpload.jsx
+++ src/components/FileUpload.jsx
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import WorkingContext from "../context/workingContext";
 
 import TxtFileUpload from "./FileUploadFragment/TxtFileUpload";
@@ -7,6 +8,12 @@ import XlsxFileUpload from "./FileUploadFragment/XlsxFileUpload";
 const FileUpload = () => {
   const { mainBtnDisabled } = useContext(WorkingContext);
 
+  const navigate = useNavigate();
+
+  const startAnalyze = () => {
+    navigate("/results");
+  };
+
   return (
     <div className="mb-4 text-center">
       <h1 className="display-4 text-center mb-4">Добро пожаловать в генератор отчетов</h1>
@@ -14,7 +21,7 @@ const FileUpload = () => {
       <TxtFileUpload />
       <XlsxFileUpload />
 
-      <button type="button" className="btn btn-primary btn-lg mt-4" disabled={!mainBtnDisabled}>
+      <button type="button" className="btn btn-primary btn-lg mt-4" disabled={!mainBtnDisabled} onClick={startAnalyze}>
         Начать анализ
       </button>
     </div>
--- src/components/resultObserve.jsx
+++ src/components/resultObserve.jsx
@@ -1,6 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const ResultObserve = () => {
+  const [allData, setAllData] = useState();
+
+  useEffect(() => {
+    getAllData();
+  }, []);
+
+  const getAllData = async () => {
+    const response = await axios.get("http://localhost:5000/");
+
+    if (response.status === 200) {
+      setAllData(response.data);
+    } else {
+      alert("Сервер лежит, поди и ты полежи");
+    }
+  };
+
+  console.log(allData);
+
   return (
     <div>
       <h1>Result Observe Page</h1>
