5674938f088c3f2abe9bd1adfeb4bd2aed46ac8f
Ахметов Рустам <ahmetov.rustam2011@gmail.com>
2022-04-21 10:59:45 +0500

    global context Txt Upload File

--- src/components/FileUploadFragment/TxtFileUpload.jsx
+++ src/components/FileUploadFragment/TxtFileUpload.jsx
@@ -1,73 +1,21 @@
-import React, { Fragment, useState, useContext, useEffect } from "react";
+import React, { Fragment, useState, useContext } from "react";
 import WorkingContext from "../../context/workingContext";
-import axios from "axios";
 
 const TxtFileUpload = () => {
-  const { txtUploaded, setTxtUploaded } = useContext(WorkingContext);
-
-  console.log("TxtFileUpload", txtUploaded);
-
-  const [txtFile, setTxtFile] = useState("");
-  const [isSelected, setIsSelected] = useState(false);
-  const [textColor, setTextColor] = useState("bg-warning");
-  const [status, setStatus] = useState("Файл не загружен");
-
-  useEffect(() => {
-    console.log("TxtFileUpload", txtUploaded);
-    setTxtUploaded(true);
-  }, []);
-
-  // Непонятно почему не работает через useState
-  let isUploaded = false;
-
-  const onChange = (e) => {
-    const file = e.target.files[0];
-
-    if (file.name.split(".").pop() === "txt") {
-      setTxtFile(file);
-      setIsSelected(true);
-    } else {
-      alert(`Приложение не поддерживает файлы формата .${file.name.split(".").pop()}`);
-      e.target.value = "";
-    }
-  };
-
-  const onSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append("files", txtFile);
-
-    try {
-      const res = await axios.post("http://localhost:5000/upload", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      });
-
-      if (res.status === 200) {
-        isUploaded = true;
-        setTextColor("bg-success");
-        setStatus("Файл успешно загружен");
-      }
-
-      setIsSelected(!isUploaded);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+  const { onChangeTxt, isSelected, onSubmitTxt, textColor, txtStatus } = useContext(WorkingContext);
 
   return (
     <Fragment>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={onSubmitTxt}>
         <div className="mb-4">
           <label htmlFor="formFileMultiple" className="form-label">
             Загрузите данные по нагрузкам в формате <b>.txt</b>
           </label>
-          <input className="form-control" type="file" id="formFileMultiple" onChange={onChange} />
+          <input className="form-control" type="file" id="formFileMultiple" onChange={onChangeTxt} />
         </div>
 
         <p className={textColor}>
-          <b>{status}</b>
+          <b>{txtStatus}</b>
         </p>
 
         <button type="submit" className="btn btn-primary btn-block mt-4" disabled={!isSelected}>
--- src/context/workingContext.js
+++ src/context/workingContext.js
@@ -1,14 +1,55 @@
 import React, { createContext, useState } from "react";
 import PropTypes from "prop-types";
+import axios from "axios";
 
 const WorkingContext = createContext();
 
 export const WorkingProvider = ({ children }) => {
+  // Txt file upload
+  const [txtFile, setTxtFile] = useState("");
+  const [isSelected, setIsSelected] = useState(false);
   const [txtUploaded, setTxtUploaded] = useState(false);
-  const [xlsxUploaded, setXlsxUploaded] = useState(false);
+  const [textColor, setTextColor] = useState("bg-warning");
+  const [txtStatus, setTxtStatus] = useState("Файл не загружен");
+
+  const onChangeTxt = (e) => {
+    const file = e.target.files[0];
+
+    if (file.name.split(".").pop() === "txt") {
+      setTxtFile(file);
+      setIsSelected(true);
+    } else {
+      alert(`Приложение не поддерживает файлы формата .${file.name.split(".").pop()}`);
+      e.target.value = "";
+    }
+  };
+
+  const onSubmitTxt = async (e) => {
+    e.preventDefault();
+    const formData = new FormData();
+    formData.append("files", txtFile);
+
+    try {
+      const res = await axios.post("http://localhost:5000/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      if (res.status === 200) {
+        setTxtUploaded(true);
+        setTextColor("bg-success");
+        setTxtStatus("Файл успешно загружен");
+      }
+
+      setIsSelected(!txtUploaded);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
-    <WorkingContext.Provider value={{ txtUploaded, setTxtUploaded, xlsxUploaded, setXlsxUploaded }}>
+    <WorkingContext.Provider value={{ onChangeTxt, onSubmitTxt, textColor, isSelected, txtStatus }}>
       {children}
     </WorkingContext.Provider>
   );
